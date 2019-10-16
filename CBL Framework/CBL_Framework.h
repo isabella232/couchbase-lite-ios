cd3ea26a034a9a19c96cff0118a2d371a388169b
Kevin Cao <kyoji2@gmail.com>
2019-10-16 14:43:14 +0800

    [WIP] save progress

---
+++ CBL Framework/CBL_Framework.h
@@ -0,0 +1,19 @@
+//
+//  CBL_Framework.h
+//  CBL Framework
+//
+//  Created by Kevin Cao on 2019/10/16.
+//  Copyright © 2019 Couchbase, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CBL_Framework.
+FOUNDATION_EXPORT double CBL_FrameworkVersionNumber;
+
+//! Project version string for CBL_Framework.
+FOUNDATION_EXPORT const unsigned char CBL_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CBL_Framework/PublicHeader.h>
+
+
